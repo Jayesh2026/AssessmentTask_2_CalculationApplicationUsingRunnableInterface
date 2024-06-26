dc3b170393612e19aa98bf97cba72ad776bf7d80
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-06-26 22:52:28 +0530

    test cases for CalculationTask

---
+++ src/test/java/com/example/service/CalculationTaskTest.java
@@ -0,0 +1,91 @@
+package com.example.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+
+@ExtendWith(MockitoExtension.class)
+public class CalculationTaskTest {
+    
+    @InjectMocks
+    CalculationTask calculationTask;
+
+    @Mock
+    private Logger loggerMock;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testSquare() {
+        assertEquals(25, calculationTask.square(5));
+        assertEquals(144, calculationTask.square(12));
+        assertEquals(0, calculationTask.square(0));
+    }
+
+    @Test
+    public void testCube(){
+        assertEquals(125, calculationTask.cube(5));
+        assertEquals(3375, calculationTask.cube(15));
+        assertEquals(0, calculationTask.cube(0));
+    }
+
+    @Test
+    public void testIsEven() {
+        assertTrue(calculationTask.isEven(2));
+        assertTrue(calculationTask.isEven(0));
+        assertFalse(calculationTask.isEven(1));
+        assertFalse(calculationTask.isEven(7));
+    }
+
+    @Test
+    public void testIsPrimeOrNot() {
+        assertFalse(calculationTask.isPrimeOrNot(1));
+        assertTrue(calculationTask.isPrimeOrNot(2));
+        assertTrue(calculationTask.isPrimeOrNot(7));
+        assertFalse(calculationTask.isPrimeOrNot(9));
+        assertTrue(calculationTask.isPrimeOrNot(11));
+    }
+
+    @Test
+    public void testIsArmstrong() {
+        assertTrue(calculationTask.isArmstrong(153));
+        assertTrue(calculationTask.isArmstrong(370));
+        assertFalse(calculationTask.isArmstrong(123));
+        assertTrue(calculationTask.isArmstrong(9474));
+    }
+
+    @Test
+    public void testIsPalindrome() {
+        assertTrue(calculationTask.isPalindrome(121));
+        assertTrue(calculationTask.isPalindrome(1331));
+        assertFalse(calculationTask.isPalindrome(123));
+        assertFalse(calculationTask.isPalindrome(1234));
+    }
+
+    @Test
+    public void testFactorial() {
+        assertEquals(120, calculationTask.factorial(5));
+        assertEquals(1, calculationTask.factorial(0));
+        assertEquals(1, calculationTask.factorial(1));
+        assertEquals(720, calculationTask.factorial(6));
+    }
+
+    @Test
+    public void testReverse() {
+        assertEquals(321, calculationTask.reverse(123));
+        assertEquals(0, calculationTask.reverse(0));
+        assertEquals(54321, calculationTask.reverse(12345));
+    }
+
+}
+
