efd0309b6d5ecf815592105ead602fe11d76afb3
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-06-26 14:54:16 +0530

    first commit

---
+++ src/main/java/com/example/CalculationTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculationTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculationTaskApplication.class, args);
+	}
+
+}
