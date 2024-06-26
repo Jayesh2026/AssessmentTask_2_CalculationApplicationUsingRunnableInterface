ae1b33d38d48a8d1a4186a002abf6d133da0d611
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-06-26 23:11:53 +0530

    update code

--- src/main/java/com/example/controller/CalculationController.java
+++ src/main/java/com/example/controller/CalculationController.java
@@ -5,12 +5,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.service.CalculationService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/calculation")
 public class CalculationController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CalculationController.class);
 
     @Autowired
     CalculationService calculationService;
@@ -30,7 +34,14 @@ public class CalculationController {
         };
 
         for (Runnable threadStart : threads) {
-            Thread thread = new Thread(threadStart);
+            Thread thread = new Thread(() -> {
+                try {
+                    threadStart.run();
+                } catch (Exception e) {
+                    logger.error("Exception occurred in thread: {}", e.getMessage());
+                    e.printStackTrace();
+                }
+            });
             thread.start();
         }
     }
