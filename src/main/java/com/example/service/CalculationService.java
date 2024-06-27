ad5ebfaa4000b154ac643f438f76c2974c1a5ac2
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-06-28 01:50:44 +0530

    update

--- src/main/java/com/example/service/CalculationService.java
+++ src/main/java/com/example/service/CalculationService.java
@@ -4,64 +4,58 @@ import java.util.Scanner;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CalculationService {
+public class CalculationService extends CalculationTask {
 
     public static final Logger logger = LoggerFactory.getLogger(CalculationService.class);
-    
-    @Autowired
-    CalculationTask calculationTask;
 
     Scanner sc = new Scanner(System.in);
     int number;
 
     public CalculationService() {
         System.out.println("Enter a  number: ");
-        number=sc.nextInt();
+        number = sc.nextInt();
     }
-    
+
     public Runnable squareTask = () -> {
-        int square = calculationTask.square(number);
+        int square = super.square(number);
         logger.info("Square of {} is: {}", number, square);
     };
 
     public Runnable cubeTask = () -> {
-        int cube = calculationTask.cube(number);
+        int cube = super.cube(number);
         logger.info("Cube of {} is: {}", number, cube);
     };
 
     public Runnable evenTask = () -> {
-        boolean even = calculationTask.isEven(number);
+        boolean even = super.isEven(number);
         logger.info(" {} is even: {}", number, even);
     };
-    
+
     public Runnable primeTask = () -> {
-        boolean isPrime = calculationTask.isPrimeOrNot(number);
-        logger.info("{} is prime: {}",number, isPrime);
+        boolean isPrime = super.isPrimeOrNot(number);
+        logger.info("{} is prime: {}", number, isPrime);
     };
 
     public Runnable armstrongTask = () -> {
-        boolean armstrong = calculationTask.isArmstrong(number);
+        boolean armstrong = super.isArmstrong(number);
         logger.info("{} is Armstrong: {}", number, armstrong);
     };
 
     public Runnable palindromeTask = () -> {
-        boolean isPalindrome = calculationTask.isPalindrome(number);
+        boolean isPalindrome = super.isPalindrome(number);
         logger.info("{} is palindrome: {}", number, isPalindrome);
     };
 
     public Runnable factorialTask = () -> {
-        int factorialNum = calculationTask.factorial(number);
+        int factorialNum = super.factorial(number);
         logger.info("Factorial of {} is: {}", number, factorialNum);
     };
 
     public Runnable reverseTask = () -> {
-        int reversedNum = calculationTask.reverse(number);
-        logger.info("Reverse of {} is: {}",number, reversedNum);
+        int reversedNum = super.reverse(number);
+        logger.info("Reverse of {} is: {}", number, reversedNum);
     };
 }
-
-
--- src/main/java/com/example/service/CalculationTask.java
+++ src/main/java/com/example/service/CalculationTask.java
@@ -1,8 +1,5 @@
 package com.example.service;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public class CalculationTask {
     
     // for square of number
